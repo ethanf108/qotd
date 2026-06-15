e9345117964b1d556ad99258f6052e8a1f207c77
Ethan Ferguson <ethanf108@gmail.com>
2026-06-14 21:25:21 -0400

    Rejiggering

--- qotd.c
+++ qotd.c
@@ -10,9 +10,9 @@
 #include <string.h>
 
 #define QOTD_PORT 17
-#define LISTEN_BACKLOG 64
-#define MAX_MESSAGE_SIZE 65535
+#define MAX_QOTD_SIZE 65535
 #define QOTD_MESSAGE_FILE "/etc/qotd.txt"
+#define LISTEN_BACKLOG 64
 #define SIMUL_EPOLL_EVENTS 2
 #define READ_BUF_SIZE 65537
 
@@ -28,7 +28,7 @@ struct sockdef {
 };
 
 static int protocols[] = {P_FOUR, P_ECHO, P_DISCARD, P_QOTD};
-static char qotd_message[MAX_MESSAGE_SIZE];
+static char qotd_message[MAX_QOTD_SIZE];
 static char read_buffer[READ_BUF_SIZE];
 
 static int read_qotd_message() {
@@ -56,10 +56,10 @@ static int read_qotd_message() {
     }
   }
 
-  ret = read(fd, qotd_message, MAX_MESSAGE_SIZE);
+  ret = read(fd, qotd_message, MAX_QOTD_SIZE);
   if (ret < 0) {
     fprintf(stderr, "Error reading QOTD file: %d\n", errno);
-  } else if (ret < MAX_MESSAGE_SIZE) {
+  } else if (ret < MAX_QOTD_SIZE) {
     qotd_message[ret] = '\0';
   }
 
@@ -140,45 +140,27 @@ static int setup_server(int tcp, int port, int epoll_fd) {
 static int handle_tcp(struct sockdef *sock_info) {
   int i, ret, pos;
 
-  switch (sock_info->protocol) {
-  case P_QOTD:
+  if (sock_info->protocol == P_QOTD) {
     read_qotd_message();
-    ret = send(sock_info->fd, qotd_message, strnlen(qotd_message, MAX_MESSAGE_SIZE), 0);
+    ret = write(sock_info->fd, qotd_message, strnlen(qotd_message, MAX_QOTD_SIZE));
     if (ret < 0) {
-      fprintf(stderr, "Error sending message to client: %d\n", errno);
+      if (errno != ECONNRESET)
+	fprintf(stderr, "Error sending message to client: %d\n", errno);
       ret = -errno;
     } else
       ret = -1; // To close it and remove from epoll
     return ret;
-  case P_DISCARD:
-    ret = read(sock_info->fd, read_buffer, READ_BUF_SIZE);
-    if (ret < 0) {
+  }
+
+  ret = read(sock_info->fd, read_buffer, READ_BUF_SIZE);
+  if (ret < 0) {
+    if (errno != ECONNRESET)
       fprintf(stderr, "Error reading from TCP port %d: %d\n", sock_info->protocol, errno);
-      return -errno;
-    } else if (ret == 0)
-      return -1;
-    else
-      return 0;
-  case P_ECHO:
-    ret = read(sock_info->fd, read_buffer, READ_BUF_SIZE);
-    if (ret < 0) {
-      fprintf(stderr, "Error reading from TCP port %d: %d\n", sock_info->protocol, errno);
-      return -errno;
-    } else if (ret == 0)
-      return -1; // EOF
-    ret = write(sock_info->fd, read_buffer, ret);
-    if (ret < 0) {
-      fprintf(stderr, "Error sending message to TCP port %d: %d\n", sock_info->protocol, errno);
-      return -errno;
-    }
-    return 0;
-  case P_FOUR:
-    ret = read(sock_info->fd, read_buffer, READ_BUF_SIZE);
-    if (ret < 0) {
-      fprintf(stderr, "Error reading from TCP port %d: %d\n", sock_info->protocol, errno);
-      return -errno;
-    } else if (ret == 0)
-      return -1;
+    return -errno;
+  } else if (ret == 0)
+    return -1;
+
+  if (sock_info->protocol == P_FOUR) {
     pos = 0;
     for (i = 0; i < ret; i++) {
       if (read_buffer[i] == '4')
@@ -186,60 +168,78 @@ static int handle_tcp(struct sockdef *sock_info) {
     }
     if (pos == 0)
       return 0;
-    ret = write(sock_info->fd, read_buffer, pos);
-    if (ret < 0) {
-      fprintf(stderr, "Error sending message to TCP port %d: %d\n", sock_info->protocol, errno);
-      return -errno;
-    }
+    ret = pos;
   }
+
+  if (sock_info->protocol == P_DISCARD)
+    return 0;
+
+  ret = write(sock_info->fd, read_buffer, ret);
+  if (ret < 0) {
+    if (errno != ECONNRESET)
+      fprintf(stderr, "Error sending message to TCP port %d: %d\n", sock_info->protocol, errno);
+    return -errno;
+  }
+  return 0;
 }
 
 static int handle_udp(struct sockdef *sock_info, struct sockaddr_in *addr, char *buf, int buflen) {
   int i, ret;
 
-  switch (sock_info->protocol) {
-  case P_QOTD:
+  if (sock_info->protocol == P_DISCARD)
+    return 0;
+  else if (sock_info->protocol == P_QOTD) {
     read_qotd_message();
-    ret = sendto(sock_info->fd, qotd_message, strnlen(qotd_message, MAX_MESSAGE_SIZE), 0, (struct sockaddr *)addr, sizeof(*addr));
-    if (ret < 0) {
-      fprintf(stderr, "Error sending UDP port %d message: %d\n", sock_info->protocol, errno);
-      return -errno;
-    }
-    return 0;
-  case P_DISCARD:
-    return 0;
-  case P_ECHO:
-    ret = sendto(sock_info->fd, buf, buflen, 0, (struct sockaddr *)addr, sizeof(*addr));
-    if (ret < 0) {
-      fprintf(stderr, "Error sending UDP port %d message: %d\n", sock_info->protocol, errno);
-      return -errno;
-    }
-    return 0;
-  case P_FOUR:
+    buf = qotd_message;
+    buflen = strnlen(buf, MAX_QOTD_SIZE);
+  } else if (sock_info->protocol == P_FOUR) {
     ret = 0;
     for (i = 0; i < buflen; i++) {
       if (buf[i] == '4')
-	buf[ret++] = '4';;
+	buf[ret++] = '4';
     }
-    ret = sendto(sock_info->fd, buf, ret, 0, (struct sockaddr *)addr, sizeof(*addr));
-    if (ret < 0) {
-      fprintf(stderr, "Error sending UDP port %d message: %d\n", sock_info->protocol, errno);
-      return -errno;
-    }
-    return 0;
-  default:
-    fprintf(stderr, "Error incorrect protocol: %d\n", sock_info->protocol);
-    return -1;
+    buflen = ret;
   }
+
+  ret = sendto(sock_info->fd, buf, buflen, 0, (struct sockaddr *)addr, sizeof(*addr));
+  if (ret < 0) {
+    fprintf(stderr, "Error sending UDP port %d message: %d\n", sock_info->protocol, errno);
+    return -errno;
+  }
+  return 0;
 }
 
-static void log_connection(struct sockaddr_in *addr, int tcp) {
+static void log_connection(struct sockaddr_in *addr, struct sockdef *sock_info) {
   in_addr_t ipaddr;
   in_port_t port;
+  char *proto_str;
+
+  switch (sock_info->protocol) {
+  case P_QOTD:
+    proto_str = "QOTD";
+    break;
+  case P_DISCARD:
+    proto_str = "DISC";
+    break;
+  case P_FOUR:
+    proto_str = "FOUR";
+    break;
+  case P_ECHO:
+    proto_str = "ECHO";
+    break;
+  }
 
   port = ntohs(addr->sin_port);
   ipaddr = ntohl(addr->sin_addr.s_addr);
-  printf("%s Connection from %d.%d.%d.%d:%d\n", tcp == 1 ? "TCP" : "UDP", (ipaddr & 0xFF000000) >> 24, (ipaddr & 0x00FF0000) >> 16, (ipaddr & 0x0000FF00) >>8, ipaddr & 0x000000FF, port, ipaddr);
+  printf("%s %s Connection from %d.%d.%d.%d:%d\n",
+	 (sock_info->flags & 1) ? "TCP" : "UDP",
+	 proto_str,
+	 (ipaddr & 0xFF000000) >> 24,
+	 (ipaddr & 0x00FF0000) >> 16,
+	 (ipaddr & 0x0000FF00) >> 8,
+	 ipaddr & 0x000000FF,
+	 port,
+	 ipaddr);
 }
 
 static void handle(struct epoll_event *event, int epoll_fd) {
@@ -265,7 +265,7 @@ static void handle(struct epoll_event *event, int epoll_fd) {
       if (accept_addr_len > sizeof(accept_addr))
 	fprintf(stderr, "Received sockaddr bigger than sizeof(struct sockaddr_in)!!! HUH ???\n");
       else {
-	log_connection(&accept_addr, 1);
+	log_connection(&accept_addr, curr_sock_info);
 	tcp_port = curr_sock_info->protocol;
 	curr_sock_info = (struct sockdef *)malloc(sizeof(struct sockdef));
 	curr_sock_info->fd = ret;
@@ -300,14 +300,14 @@ static void handle(struct epoll_event *event, int epoll_fd) {
     if (accept_addr_len > sizeof(accept_addr))
       fprintf(stderr, "Received sockaddr bigger than sizeof(struct sockaddr_in)!!! HUH ??? NO MESSAGE SENT\n");
     else {
-      log_connection(&accept_addr, 0);
+      log_connection(&accept_addr, curr_sock_info);
       handle_udp(curr_sock_info, &accept_addr, read_buffer, ret);
     }
   }
 }
 
 int main (int argc, char **argv) {
-  int i, ret, epoll_fd, epoll_num, tcp_port;
+  int i, ret, epoll_fd;
   struct epoll_event events[SIMUL_EPOLL_EVENTS];
 
   ret = setvbuf(stdout, NULL, _IOLBF, 0);
@@ -344,15 +344,15 @@ int main (int argc, char **argv) {
   }
 
   while (1) {
-    epoll_num = epoll_wait(epoll_fd, events, SIMUL_EPOLL_EVENTS, -1);
-    if (epoll_num < 0) {
+    ret = epoll_wait(epoll_fd, events, SIMUL_EPOLL_EVENTS, -1);
+    if (ret < 0) {
       if (errno == EINTR)
 	continue;
       fprintf(stderr, "Error while polling server sockets: %d\n", errno);
       return 1;
     }
 
-    for (i = 0; i < epoll_num; i++) {
+    for (i = 0; i < ret; i++) {
       handle(&events[i], epoll_fd);
     }
   }
